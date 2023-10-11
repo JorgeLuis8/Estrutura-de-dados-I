de815a658853a0161d98693556a7429987144b90
JorgeLuis8 <jorge.luz@ufpi.edu.br>
2023-10-11 16:36:50 -0300

    primeira atvd sala

--- atvd_sala/primeira.c
+++ atvd_sala/primeira.c
@@ -3,23 +3,42 @@
 #include <time.h>
 
 int main(){
-    int *vet,n,i;
+    int *vet,n,i,soma,media,x;
+    int max,min = 10000;
     srand(time(NULL));
 
     printf("Digite o tamanho do vetor: ");
     scanf("%d",&n);
 
-    vet = (int *) malloc(sizeof(int) * n);
+    vet = (int *) malloc(n*sizeof(int));
 
-    for(i = 0; i < n; i++){
-        vet[i] = rand() % 100;
+  if (!vet)
+    {
+        printf("Memory error");
+        exit(1);
     }
 
-    for (i = 0; i < n; i++){
-        printf("%d ", vet[i]);
+    for (i = 0; i<n; i++)
+    {
+        vet[i] = rand()%n;
     }
-
-
     
+    for (i = 0; i< n; i++){
+		soma = soma + vet[i];
+	}
+    
+    for (i = 0; i <n ; i++){
+		printf("%d\n ",vet[i]);
+	}
+	
+	media = (float) soma/n;
+	
+	printf("%d",media);
+	
+	
+	
+
+
+
     return 0;
 }
