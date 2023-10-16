7063fa365932368bf4fac2e7772aaa429310707f
JorgeLuis8 <jorge.luz@ufpi.edu.br>
2023-10-16 09:03:20 -0300

    primeira questão feita

--- atvd_sala/primeira.c
+++ atvd_sala/primeira.c
@@ -3,8 +3,11 @@
 #include <time.h>
 
 int main(){
-    int *vet,n,i,soma = 0,media = 0;
-    srand(time(NULL));
+    int *vet,n,i,soma = 0,max = 0,min = 1000000;
+    double media;
+    float *normal;
+    srand((unsigned)time(NULL));
+
 
     printf("Digite o tamanho do vetor: ");
     scanf("%d",&n);
@@ -27,13 +30,37 @@ int main(){
 	}
     for (i = 0; i <n ; i++){
 		soma = soma + vet[i];
-        media = (float) soma / n;
 	}
-
+    media = (float) soma / n;
+    printf("A soma dos valores do vetor eh %d\n",soma);
     printf("A media dos valores do vetor eh: ");
-    printf("%d\n", media);
+    printf("%.2f\n", media);
 
+    for(i = 0; i<n;i++){
+        if(vet[i] > max){
+            max = vet[i];
+        }
+        if (vet[i] < min)
+        {
+            min = vet[i];
+        }
+        
+    }
+    printf("A maior posicao do vetor eh:  %d\n",max);
+    printf("A menor posicao do vetor eh:  %d\n",min);
 
+    normal = (float *) calloc(n,sizeof(float));
+
+    for (i = 0; i < n; i++){
+        normal[i] = (vet[i]-min)/(max - min);
+
+    }
+
+    for (i = 0; i <n ; i++){
+        printf("%.2f\n",normal[i]);
+    }
+    free(vet);
+    free(normal);
 
     return 0;
 }
