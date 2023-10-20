91b99a7b9130765e8dd2ff6f98c8968c45366277
Jorge Luis <jorge.luz@ufpi.edu.br>
2023-10-20 17:09:42 -0300

    questão feita

--- Atvd2/2.c
+++ Atvd2/2.c
@@ -1,59 +1,60 @@
+/*Crie um algoritmo que recebe entradas inteiras do usuário até um momento que ele
+digitar um valor X que servirá como parada. X = -1, lembrando que o valor de X não pode
+ser contabilizado para nada (somatório, média, menor valor, maior valor...). Armazenar
+todos os valores em um vetor alocado dinamicamente, neste caso, reflita sobre qual
+função melhor se adequa para este problema. Ao final, o algoritmo deverá informar:
+a. O valor do somatório de todos os números;
+b. O valor da média de todos os números;
+c. O menor e o maior números informados;
+d. mostrar os valores contidos no vetor;
+
+Ao final, lembre-se de liberar a memória utilizada. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
+    int *pointer = NULL;
+    int x, i , count = 0, soma = 0, menor = 1000000, maior = 0;
 
-    int x, i,soma=0, count = 0,max = 0,min = 1000000;
-    float media=0;
-    int *pointer;
-
-    while (1)
-    {
+    while (1){
+        printf("Informe um numero (para parar digite : -1): ");
         scanf("%d", &x);
 
-        if (x == -1)
-        {
+        if (x == -1){
             break;
         }
 
+        pointer = (int *)realloc(pointer, (count + 1) * sizeof(int));
+        pointer[count] = x;
+
         count++;
-        pointer = (int *)malloc(count * sizeof(int));
-         if (!pointer){
-        printf("Memory error");
-        exit(1);
-    }
-        for (i = 0; i < count; i++)
-        {
-            pointer[i] = x;
+        soma += x;
+        if (x < menor){
+            menor = x;
+        }
+        if (x > maior){
+            maior = x;
         }
     }
-    for (i = 0; i < count; i++)
-    {
-        printf("%d\n",pointer[i]) ;
-    }
 
-    for (i = 0; i < count; i++)
-    {
-        soma += pointer[i];
-    }
-    media = (float) soma/ count;
-
-      for(i = 0; i<count;i++){
-        if(pointer[i] > max){
-            max = pointer[i];
+  
+        printf("Valores contidos no vetor: ");
+        for (i = 0; i < count; i++){
+            printf("%d ", pointer[i]);
         }
-        if (pointer[i] < min)
-        {
-            min = pointer[i];
-        }
-        
-    }
-    printf("A maior posicao do vetor eh:  %d\n",max);
-    printf("A menor posicao do vetor eh:  %d\n",min);
+        printf("\n");
 
+        printf("A soma dos valores eh: %d\n", soma);
+        printf("A media dos valores eh: %.2f\n", (float)soma / count);
+        printf("O menor numero eh: %d\n", menor);
+        printf("O maior numero eh: %d\n", maior);
+ 
 
-    printf("Quantidade de numeros digitados: %d\n", count);
-    printf("A media dos valores do vetor eh: %.2f\n", media);
+    free(pointer);
+ 
+
     return 0;
 }
