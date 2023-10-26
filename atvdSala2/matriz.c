d0815af1fb86c446c4cc150738882d6673855a38
Jorge Luis <jorge.luz@ufpi.edu.br>
2023-10-26 16:21:02 -0300

    atts

--- atvdSala2/matriz.c
+++ atvdSala2/matriz.c
@@ -21,7 +21,7 @@ float acessar(Matriz *m,int n, int l){
     int i,j;
     for(i = 0; i<n;i++){
         for(j = 0;j < l; i++){
-            printf("%f", (m -> v + (i*n) + j));
+            printf("%f", *(m -> v + (i*n) + j));
         }
     }
 }
