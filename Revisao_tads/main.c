be01ad0563ccbf38994d9d4ba33e74a396818c2b
JorgeLuis8 <jorge.luz@ufpi.edu.br>
2023-11-06 22:32:40 -0300

    atvds

---
+++ Revisao_tads/main.c
@@ -0,0 +1,20 @@
+#include "jogador.c"
+
+int main(){
+     int numJogadores;
+    printf("Quantos jogadores deseja cadastrar? ");
+    scanf("%d", &numJogadores);
+
+    Jogador *jogadores = atrubuir(numJogadores);
+
+    if (jogadores) {
+        imprimir(jogadores, numJogadores);
+        printf("Soma dos pontos de todos os jogadores: %.2f\n", soma(jogadores, numJogadores));
+        ehBom(jogadores, numJogadores);
+        liberar(jogadores);
+    } else {
+        printf("Falha na alocação de memória.\n");
+    }
+
+    return 0;
+}
