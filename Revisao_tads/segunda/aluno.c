5a7b03468702cb41f174787edb57dfb32b4457b2
JorgeLuis8 <jorge.luz@ufpi.edu.br>
2023-11-07 21:46:40 -0300

    Terminando uma questão e iniciando outras

--- Revisao_tads/segunda/aluno.c
+++ Revisao_tads/segunda/aluno.c
@@ -20,7 +20,7 @@ Aluno *atribuir(int num) {
     for (i = 0; i < num; i++) {
         printf("Digite o nome do aluno: ");
         scanf("%s", a[i].nome);
-        printf("Digite a matrícula do aluno: ");
+        printf("Digite a matricula do aluno: ");
         scanf("%d", &a[i].matricula);
         printf("Digite a nota 1 do aluno: ");
         scanf("%f", &a[i].nota1);
@@ -38,9 +38,39 @@ void imprimir(Aluno *a, int num) {
     for (i = 0; i < num; i++) {
         printf("O nome do aluno eh: %s\n", a[i].nome);
         printf("A matricula do aluno eh: %d\n", a[i].matricula);
-        printf("A nota 1 do aluno eh: %f\n", a[i].nota1);
-        printf("A nota 2 do aluno eh: %f\n", a[i].nota2);
-        printf("A nota 3 do aluno eh: %f\n", a[i].nota3);
+        printf("A nota 1 do aluno eh: %.2f\n", a[i].nota1);
+        printf("A nota 2 do aluno eh: %.2f\n", a[i].nota2);
+        printf("A nota 3 do aluno eh: %.2f\n", a[i].nota3);
         printf("\n");
     }
+}
+ void media(Aluno *a,int qtd_aluno){
+    float soma=0,media;
+    int i;
+    for(i=0;i<qtd_aluno;i++){
+        soma = a[i].nota1 + a[i].nota2 + a[i].nota3;
+        media = soma/(3);
+        printf("A media do aluno: %s eh = %.2f\n",a[i].nome,media);
+    }
+    
+}
+void aprovacao(Aluno *a,int num){
+    int i;
+    float media_limite = 6;
+    float media,soma=0 ;
+
+    for(i=0;i<num;i++){
+        soma = a[i].nota1 + a[i].nota2 + a[i].nota3;
+        media = soma/(3);
+        if (media >= media_limite){
+            printf("O aluno %s foi aprovado\n",a[i].nome);
+        }else{
+            printf("O aluno %s foi reprovado\n",a[i].nome);
+        }
+        
+        
+    }
+}
+void liberar(Aluno *a){
+    free(a);
 }
--- Revisao_tads/segunda/aluno.h
+++ Revisao_tads/segunda/aluno.h
@@ -1,3 +1,6 @@
 typedef struct aluno Aluno;
 Aluno* atribuir(int numaluno);
 void imprimir(Aluno *a,int numaluno);
+void media(Aluno *a,int numaluno);
+void aprovacao(Aluno *a,int numaluno);
+void liberar(Aluno *a);
