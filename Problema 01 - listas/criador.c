9863cfbfecfb7332d0d6c5854339e97aa76a0fa9
JorgeLuis8 <jorge.luz@ufpi.edu.br>
2024-01-03 14:53:07 -0300

    atts

--- Problema 01 - listas/criador.c
+++ Problema 01 - listas/criador.c
@@ -8,49 +8,63 @@ struct criador
 {
 	int id_criador;
 	char nome[100];
-	Fazenda *fazendas;//Esse ponteiro e uma lista circular (atentem-se)
-	float patrimonio; //esse valor deve ser constantemente modificado para que o valor esteja atualizado	
-	struct criador *prox, *ant; //lista dupla encadeada, nao é necessário ser circular
+	Fazenda *fazendas;			// Esse ponteiro e uma lista circular (atentem-se)
+	float patrimonio;			// esse valor deve ser constantemente modificado para que o valor esteja atualizado
+	struct criador *prox, *ant; // lista dupla encadeada, nao é necessário ser circular
 };
 
-Criador *criarListaDuplaCriadores(){
+Criador *criarListaDuplaCriadores()
+{
 	return NULL;
 }
 
-Criador *cadastrar(Criador *criadores){
-	Criador *novo = (Criador*) malloc(sizeof(Criador));
-	if (novo == NULL) {
+Criador *cadastrar(Criador *criadores)
+{
+	Criador *novo = (Criador *)malloc(sizeof(Criador));
+	if (novo == NULL)
+	{
 		printf("Erro ao alocar memoria\n");
 		exit(1);
 	}
 	printf("Informe o id do criador: ");
-	scanf("%d",&novo->id_criador);
-	//printf("Informe o nome do criadouro: ");
-	//scanf("%s",novo->nome);
+	scanf("%d", &novo->id_criador);
+	// printf("Informe o nome do criadouro: ");
+	// scanf("%s",novo->nome);
 
-	if (criadores == NULL || criadores->id_criador > novo->id_criador){
+	if (criadores == NULL || criadores->id_criador > novo->id_criador)
+	{
 		novo->prox = criadores;
 		novo->ant = criadores;
 		criadores = novo;
-	}else{
-			Criador *aux = criadores;
-			while (aux->prox != NULL && aux->prox->id_criador < novo->id_criador){
-				aux = aux->prox;
-			}
-			aux->prox = novo;
-			novo->ant = aux;
-			novo->prox = NULL;
-
+	}
+	else
+	{
+		Criador *aux = criadores;
+		while (aux->prox != NULL && aux->prox->id_criador < novo->id_criador)
+		{
+			aux = aux->prox;
 		}
-	
+		novo->prox = aux->prox;
+
+		if (aux->prox != NULL)
+		{
+			aux->prox->ant = novo;
+		}
+
+		aux->prox = novo;
+		novo->ant = aux;
+	}
+
 	return criadores;
 }
 
-void imprimir(Criador *criadores) {
+void imprimir(Criador *criadores)
+{
 	Criador *aux = criadores;
-	while (aux != NULL) {
+	while (aux != NULL)
+	{
 		printf("id: %d\n", aux->id_criador);
-		//printf("id da fazenda: %s\n", aux->nome);
+		// printf("id da fazenda: %s\n", aux->nome);
 		aux = aux->prox;
 	}
 }
